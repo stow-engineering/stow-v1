de6b572e75094cdbb4cb5db017f35aa0582414e6
Tony Bertolino <tony.bertolino@utexas.edu>
2022-04-15 04:49:33 -0500

    updates esp that container is registered

--- stow/lib/add_container.dart
+++ stow/lib/add_container.dart
@@ -15,6 +15,7 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'container_chart.dart';
 import 'container.dart' as customContainer;
 import 'user_containers.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class AddContainer extends StatefulWidget {
   final AddContainerArg arg;
@@ -96,7 +97,23 @@ class _AddContainerState extends State<AddContainer> {
                         service.updateContainerData(
                             name, size!, widget.arg.mac);
                         service.updateContainers(widget.arg.mac);
-                        setState(() {});
+                        setState(() async {
+                          List<BluetoothDevice> devices =
+                              await FlutterBluePlus.instance.connectedDevices;
+                          BluetoothDevice dev = devices.firstWhere(
+                              (d) => d.id.toString() == widget.arg.mac);
+                          List<BluetoothService> services =
+                              await dev.discoverServices();
+                          BluetoothService serv = services.firstWhere((s) =>
+                              s.uuid.toString() ==
+                              "2d8bdb4c-8be8-4980-a066-4f531f08c626");
+                          BluetoothCharacteristic char = serv.characteristics
+                              .firstWhere((c) =>
+                                  c.uuid.toString() ==
+                                  "a0edbb2a-405d-4331-8540-7afaf0e934b9");
+                          await char.write("registered".codeUnits,
+                              withoutResponse: true);
+                        });
                       },
                       child: const Text(
                         'Create my container!',
