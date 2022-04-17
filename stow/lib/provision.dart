d896c81d7d7cafca445cafda531c4f5cf7932d23
Tony Bertolino <tony.bertolino@utexas.edu>
2022-04-17 06:19:42 -0500

    seperated the provisioning process

--- stow/lib/provision.dart
+++ stow/lib/provision.dart
@@ -13,8 +13,8 @@ import 'widgets.dart';
 
 //Place UUID varibales here, not sure if this creates problems
 //Guid stowServiceUUID = Guid("2d8bdb4c-8be8-4980-a066-4f531f08c626");
-String ssid = "LAWDAWG";
-String pw = "ilovepizza";
+String ssid = "";
+String pw = "";
 
 class Provision extends StatefulWidget {
   final StowUser user;
@@ -97,7 +97,7 @@ class _PairScreenState extends State<PairScreen> {
         );
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Connect and Register Stow Device"),
+        title: const Text('Select Devices'),
         actions: [
           ElevatedButton(
               child: const Text('Connect Devices'),
@@ -145,42 +145,6 @@ class _PairScreenState extends State<PairScreen> {
                         }
                       }));
                 });
-                //ssid = (await openDialog('SSID'))!;
-                //pw = (await openDialog('Password'))!;
-                /*
-                devices.map((e) => () async {
-                      List<BluetoothService> services =
-                          await e.discoverServices();
-                      services
-                          .map((s) => s.characteristics.map((c) => () async {
-                                if (c.uuid.toString() ==
-                                    "a0edbb2a-405d-4331-8540-7afaf0e934b9") {
-                                  await c.write(ssid.codeUnits,
-                                      withoutResponse: true);
-                                } else if (c.uuid.toString() ==
-                                    "7b6cb0d3-1a7a-4ce1-92bd-f4b8d5d580d9") {
-                                  await c.write(pw.codeUnits,
-                                      withoutResponse: true);
-                                }
-                              }));
-                    });
-                    */
-                /*
-                  devices.map((d) => d.services.map((s) => s
-                      .where((q) =>
-                          q.uuid.toString() ==
-                          "2d8bdb4c-8be8-4980-a066-4f531f08c626")
-                      .map((e) => e.characteristics.map((c) => () async {
-                            if (c.uuid.toString() ==
-                                "a0edbb2a-405d-4331-8540-7afaf0e934b9") {
-                              await c.write(ssid.codeUnits,
-                                  withoutResponse: true);
-                            } else {
-                              await c.write(pw.codeUnits,
-                                  withoutResponse: true);
-                            }
-                          }))));
-                          */
               }),
         ],
       ),
@@ -271,15 +235,32 @@ class _PairScreenState extends State<PairScreen> {
                                     }
                                   : null,
                               child: Text("Select")),
-                          /*onTap: () async {
-                            if (await r.device.state == '1')
-                              await r.device.connect();
-                          },
-                          */
                         ))
                     .toList(),
               ),
             ),
+            if (idRef.isNotEmpty)
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: idRef.isNotEmpty
+                      ? () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return ConnectScreen(user: widget.user);
+                          }));
+                        }
+                      : null,
+                  child: const Text(
+                    'Continue',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
           ],
         )),
       ),
@@ -301,6 +282,181 @@ class _PairScreenState extends State<PairScreen> {
                     timeout: const Duration(seconds: 4)));
           }
         },
+      ),
+    );
+  }
+}
+
+class ConnectScreen extends StatefulWidget {
+  final StowUser user;
+  const ConnectScreen({Key? key, required this.user}) : super(key: key);
+
+  @override
+  State<ConnectScreen> createState() => _ConnectScreenState();
+}
+
+class _ConnectScreenState extends State<ConnectScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool bssid = false;
+  bool pass = false;
+  String ssid = "";
+  String password = "";
+  List<String> idRef = [];
+  List<bool> isSyncing = [];
+  List<bool> isSuccess = [];
+  List<bool> isNotified = [];
+  List<bool> isRegisterReady = [];
+  @override
+  Widget build(BuildContext context) {
+    //final ssidController = TextEditingController();
+    //final passController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Connect Devices"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 30.0, right: 30.0, top: 25.0, bottom: 0),
+                    child: TextFormField(
+                      key: Key(ssid),
+                      initialValue: ssid,
+                      //controller: ssidController,
+                      decoration: const InputDecoration(
+                        hintText: 'WiFi Name',
+                      ),
+                      onTap: () {
+                        bssid = true;
+                        //ssid = ssidController.text;
+                        setState(() {});
+                      },
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Enter a WiFi name';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 30.0, right: 30.0, top: 25.0, bottom: 0),
+                    child: TextFormField(
+                      key: Key(password),
+                      initialValue: password,
+                      //controller: passController,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                      onTap: () {
+                        pass = true;
+                        //password = passController.text;
+                        setState(() {});
+                      },
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Enter Password';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(
+                height: 40,
+                thickness: 2,
+                indent: 150,
+                endIndent: 150,
+                color: Colors.grey),
+            StreamBuilder<List<BluetoothDevice>>(
+              stream: Stream.periodic(const Duration(seconds: 4))
+                  .asyncMap((_) => FlutterBluePlus.instance.connectedDevices),
+              initialData: const [],
+              builder: (c, snapshot) => Column(
+                children: snapshot.data!
+                    .map((d) => ListTile(
+                          title: Text(d.name),
+                          subtitle: Text(d.id.toString()),
+                          trailing: StreamBuilder<BluetoothDeviceState>(
+                            stream: d.state,
+                            initialData: BluetoothDeviceState.disconnected,
+                            builder: (c, snapshot) {
+                              if (!idRef.contains(d.id.toString())) {
+                                idRef.add(d.id.toString());
+                                isSyncing.add(false);
+                                isNotified.add(false);
+                                isSuccess.add(false);
+                                isRegisterReady.add(false);
+                              }
+                              if (snapshot.data ==
+                                      BluetoothDeviceState.connected &&
+                                  !isSyncing[idRef.indexOf(d.id.toString())]) {
+                                return Text(
+                                    "Ready to Connect Stow Device to WiFi");
+                              } else if (isSyncing[
+                                  idRef.indexOf(d.id.toString())]) {
+                                if (isNotified[
+                                    idRef.indexOf(d.id.toString())]) {
+                                  if (isSuccess[
+                                      idRef.indexOf(d.id.toString())]) {
+                                    return ElevatedButton(
+                                        onPressed: () => {
+                                              Navigator.of(context).pushNamed(
+                                                '/add_container',
+                                                arguments: AddContainerArg(
+                                                    widget.user,
+                                                    d.id.toString()),
+                                              )
+                                            },
+                                        child: Text("Register"));
+                                  } else {
+                                    return Text("Connection Failed, Try Again");
+                                  }
+                                }
+                                return Text("Connecting");
+                              }
+                              return Text(snapshot.data.toString());
+                            },
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ),
+            Container(
+              height: 40,
+            ),
+            if (bssid == true && pass == true)
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: idRef.isNotEmpty
+                      ? () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return ConnectScreen(user: widget.user);
+                          }));
+                        }
+                      : null,
+                  child: const Text(
+                    'Continue',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
