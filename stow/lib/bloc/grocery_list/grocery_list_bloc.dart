db241e391f42826674b6c0ad87dc04008c50a427
MandrewWhit <andrew.whitaker987@gmail.com>
2022-11-11 18:50:53 -0600

    grocery list rebase 4

--- stow/lib/bloc/grocery_list/grocery_list_bloc.dart
+++ stow/lib/bloc/grocery_list/grocery_list_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:stow/bloc/food/food_events.dart';
 import 'package:stow/bloc/food/food_state.dart';
+<<<<<<< HEAD
 import 'package:stow/bloc/grocery_list/grocery_list_events.dart';
 import 'package:stow/bloc/grocery_list/grocery_list_state.dart';
 import 'package:stow/models/grocery_lists.dart';
@@ -84,10 +85,44 @@ class GroceryListBloc extends Bloc<GroceryListEvents, GroceryListState> {
           status: GroceryListStatus.success, groceryLists: userGroceryLists));
     } catch (error, stacktrace) {
       print(stacktrace);
+=======
+import 'package:stow/utils/firebase.dart';
+import '../../models/food_item.dart';
+
+class FoodItemsBloc extends Bloc<FoodItemsEvents, FoodItemsState> {
+  FoodItemsBloc({
+    required this.service,
+    this.newFoodItem,
+  }) : super(const FoodItemsState()) {
+    on<LoadFoodItems>(_mapLoadEventToState);
+    on<AddFoodItem>(_mapAddEventToState);
+    on<DeleteFoodItems>(_mapDeleteEventToState);
+    on<UpdateFoodItems>(_mapUpdateEventToState);
+  }
+  final FirebaseService service;
+  final FoodItem? newFoodItem;
+
+  void _mapLoadEventToState(
+      LoadFoodItems event, Emitter<FoodItemsState> emit) async {
+    emit(state.copyWith(status: FoodItemsStatus.loading));
+    try {
+      final foodItems = await service.getFoodItemList();
+      emit(
+        state.copyWith(
+          status: FoodItemsStatus.success,
+          foodItems: foodItems,
+          numItems: foodItems!.length,
+        ),
+      );
+    } catch (error, stacktrace) {
+      print(stacktrace);
+      emit(state.copyWith(status: FoodItemsStatus.error));
+>>>>>>> 958c757 (backend grocery list updates)
     }
   }
 
   void _mapAddEventToState(
+<<<<<<< HEAD
       AddToGroceryList event, Emitter<GroceryListState> emit) async {
     emit(state.copyWith(status: GroceryListStatus.loading));
     try {
@@ -149,10 +184,36 @@ class GroceryListBloc extends Bloc<GroceryListEvents, GroceryListState> {
     } catch (error, stacktrace) {
       print(stacktrace);
       emit(state.copyWith(status: GroceryListStatus.error));
+=======
+      AddFoodItem event, Emitter<FoodItemsState> emit) async {
+    emit(state.copyWith(status: FoodItemsStatus.loading));
+    try {
+      List<FoodItem> newFoodItemsList = state.foodItems;
+      DocumentReference food_uid = await service.updateFoodItemData(
+          event.foodItem.name, event.foodItem.expDate);
+      service.updateFoodItems(food_uid.id);
+      FoodItem newFoodItem = FoodItem(
+          uid: food_uid.id,
+          barcode: event.foodItem.barcode,
+          name: event.foodItem.name,
+          value: event.foodItem.value,
+          expDate: event.foodItem.expDate);
+      newFoodItemsList.add(newFoodItem);
+      emit(
+        state.copyWith(
+            status: FoodItemsStatus.success,
+            foodItems: newFoodItemsList,
+            numItems: newFoodItemsList.length),
+      );
+    } catch (error, stacktrace) {
+      print(stacktrace);
+      emit(state.copyWith(status: FoodItemsStatus.error));
+>>>>>>> 958c757 (backend grocery list updates)
     }
   }
 
   void _mapDeleteEventToState(
+<<<<<<< HEAD
       DeleteGroceryList event, Emitter<GroceryListState> emit) async {
     emit(state.copyWith(status: GroceryListStatus.loading));
     try {
@@ -171,6 +232,48 @@ class GroceryListBloc extends Bloc<GroceryListEvents, GroceryListState> {
     } catch (error, stacktrace) {
       print(stacktrace);
       emit(state.copyWith(status: GroceryListStatus.error));
+=======
+      DeleteFoodItems event, Emitter<FoodItemsState> emit) async {
+    emit(state.copyWith(status: FoodItemsStatus.loading));
+    try {
+      List<FoodItem> newFoodItemsList = state.foodItems;
+      newFoodItemsList.remove(event.foodItem);
+      service.deleteFoodItems(event.foodItem.uid);
+      emit(
+        state.copyWith(
+            status: FoodItemsStatus.success,
+            foodItems: newFoodItemsList,
+            numItems: newFoodItemsList.length),
+      );
+    } catch (error, stacktrace) {
+      print(stacktrace);
+      emit(state.copyWith(status: FoodItemsStatus.error));
+    }
+  }
+
+  void _mapUpdateEventToState(
+      UpdateFoodItems event, Emitter<FoodItemsState> emit) async {
+    emit(state.copyWith(status: FoodItemsStatus.loading));
+    try {
+      List<FoodItem> newFoodItemsList = state.foodItems;
+      for (int i = 0; i < newFoodItemsList.length; i++) {
+        if (newFoodItemsList[i].uid == event.foodItem.uid) {
+          newFoodItemsList[i].name = event.foodItem.name;
+          newFoodItemsList[i].expDate = event.foodItem.expDate;
+        }
+      }
+      var food_id = service.updateExistingFoodItem(event.foodItem);
+      service.updateFoodItems(food_id.toString());
+      emit(
+        state.copyWith(
+            status: FoodItemsStatus.success,
+            foodItems: newFoodItemsList,
+            numItems: newFoodItemsList.length),
+      );
+    } catch (error, stacktrace) {
+      print(stacktrace);
+      emit(state.copyWith(status: FoodItemsStatus.error));
+>>>>>>> 958c757 (backend grocery list updates)
     }
   }
 }
